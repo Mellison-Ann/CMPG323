1e582f5fa0dba144043e24c60ddbdd055aa9c251
Mellison-Ann <53425655+Mellison-Ann@users.noreply.github.com>
2020-11-23 22:45:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+// set up express
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+
+// set up mongoose
+
+//ItiRSnbLBxMUSuw3
+//mongodb+srv://User:<password>@cluster0.sxen2.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+mongoose.connect(
+  process.env.MONGODB_CONNECTION_STRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB connection established");
+  }
+);
+
+// set up routes
+
+app.use("/users", require("./routes/userRouter"));
